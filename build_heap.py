29479e2c58e351fcaec959d89742acf8fece8c43
Mariss Maris Vizulis <118078188+mariiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-03-16 13:26:27 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,12 +2,30 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    for i in range (n // 2, -1, -1):
+        sift_down(i, data, swaps)
 
     return swaps
+
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_index = i
+    l = 2*i+1
+    if l<n and data[l]< data[min_index]:
+        min_index = l
+        
+    r = 2*i+2
+    if r < n and data[r]<data[min_index]:
+        min_index = r
+        
+    if i!=min_index:
+        swaps.append((i, min_index))
+        data[i],data[min_index]=data[min_index],data[i]
+        sift_down(min_index,data,swaps)
 
 
 def main():
